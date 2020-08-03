882ea09486e1553b9d71086f56e0b77d0a858c4c
Stefani Baker <stefanibaker@MacBook-Air-4.local>
2020-08-03 13:48:25 -0400

    Added Home page

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Image from "react-bootstrap/Image";
+import Container from "react-bootstrap/Container";
+
+function Home() {
+  return (
+    <div>
+      <Container>
+        <Image src="https://images.unsplash.com/photo-1524250426644-e24b385c291a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1534&q=80"></Image>
+      </Container>
+    </div>
+  );
+}
+
+export default Home;
