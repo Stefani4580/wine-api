eb568a500f4b15d4ba0afca280c7dc829f101a09
Stefani Baker <stefanibaker@MacBook-Air-4.local>
2020-08-03 14:21:29 -0400

    Adding API with hooks

--- src/components/Wines.js
+++ src/components/Wines.js
@@ -1,40 +1,52 @@
-import React from 'react';
-import './App.css';
-import Books from './components/Books';
-import Home from './components/Home';
+import React from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
+  Link,
+  useRouteMatch
 } from "react-router-dom";
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Router>
-          <div>
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/books">Books</Link>
-              </li>
-            </ul>
-            <hr />
-            <Switch>
-              <Route exact path="/">
-                <Home />
-              </Route>
-              <Route path="/books">
-                <Books />
-              </Route>
-            </Switch>
-          </div>
-        </Router>
-      </header>
-    </div>
-  );
+import Wine from "./Wine";
+
+const Wines = (props) => {
+
+    let { path, url } = useRouteMatch();
+    const [data, setData] = useState(null);
+
+    async function getWines() {
+      try {
+        const response = await axios.get("http://myapi-profstream.herokuapp.com/api/6054d6/wines");
+        setData(response.data);
+      } catch(e) {
+        console.error(e);
+      }
+    }
+
+// Execute this function just once to populate the Wine tree.
+    useEffect(() => {
+        getWines();
+    }, [])
+
+
+
+    return (
+      <div>
+        <h1>Wines</h1>
+        <ul>
+          { data ? data.map(book => <li key={ book.id }><Link to={ `${url}/${book.id}` }>{ book.title }</Link></li>): "Loading..."}
+        </ul>
+        <Switch>
+          <Route exact path={path}>
+            <h3>Choose a book in the list above</h3>
+          </Route>
+          <Route path={`${path}/:bookId`}>
+            <Book />
+          </Route>
+        </Switch>
+      </div>
+    )
 }
-export default App;
+
+export default Wines;
