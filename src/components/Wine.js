4553e49e47d9b3061e79762d3de982c4d1c75954
Stefani Baker <stefanibaker@MacBook-Air-4.local>
2020-08-03 19:22:21 -0400

    Still making corrections

--- src/components/Wine.js
+++ src/components/Wine.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 import {
     useParams
 } from "react-router-dom";
+import WineDetails from "./WineDetails";
 
 const Wine = () => {
     let { wineId } = useParams();
@@ -21,49 +22,14 @@ const Wine = () => {
     
       useEffect(() => {
         getWineByID();
+      },[])
 
 
     return (
         <div>
-            
+           <WineDetails name={data.name} year={data.year} grapes={data.grapes} country={data.country} region={data.region} desc={data.description} pic={data.picture} price={data.price} /> 
         </div>
-    )
+        )
 }
 
-export default Wine
-
-
-const base_url = "http://myapi-profstream.herokuapp.com/api/91f55d/books";
-function BookDetails(props) {
-    return (
-        <div>
-            <h1>Book title: { props.book.title }</h1>
-            <p>This book was written by { props.book.author } and released in the year of "{ props.book.release_date }"</p>
-        </div>
-    )
-}
-
-
-
-
-function Book() {
-    let { bookId } = useParams();
-    const [data, setData] = useState(null);
-    async function getBook() {
-        try {
-            const res = await axios.get(`${base_url}/${bookId}`);
-            setData(res.data);
-        } catch(e) {
-            console.error(e);
-        }
-    }
-    useEffect(() => {
-        getBook();
-    })
-    return(
-        <div>
-            { data ? <BookDetails book={ data } /> : "Retrieving book details..." }
-        </div>
-    )
-}
-export default Book;
+export default Wine;
