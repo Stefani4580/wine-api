70412007fa418ed32f11cd6ff4cc412392064998
Stefani Baker <stefanibaker@MacBook-Air-4.local>
2020-08-03 19:27:20 -0400

    added imports

--- src/components/WineDetails.js
+++ src/components/WineDetails.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ListGroup from "react-bootstrap/ListGroup";
 import Card from "react-bootstrap/Card";
-import ListGroup from "react-bootstrap/ListGroup";
+import ListGroupItem from "react-bootstrap/ListGroupItem";
 
 function WineDetails(props) {
   return (
