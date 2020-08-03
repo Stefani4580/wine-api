c9236c3e4aab31f11f3974fc7519278adbed8038
Stefani Baker <stefanibaker@MacBook-Air-4.local>
2020-08-03 19:48:14 -0400

    Had to adjust for null data

--- src/components/Wine.js
+++ src/components/Wine.js
@@ -1,35 +1,44 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import {
-    useParams
-} from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
 import WineDetails from "./WineDetails";
 
 const Wine = () => {
-    let { wineId } = useParams();
-    const [data, setData] = useState(null);
+  let { wineId } = useParams();
+  const [data, setData] = useState(null);
 
-    async function getWineByID() {
-        try {
-          const response = await axios.get(
-            `http://myapi-profstream.herokuapp.com/api/6054d6/wines/${wineId}`
-          );
-          setData(response.data);
-        } catch (e) {
-          console.error(e);
-        }
-      }
-    
-      useEffect(() => {
-        getWineByID();
-      },[])
+  async function getWineByID() {
+    try {
+      const response = await axios.get(
+        `http://myapi-profstream.herokuapp.com/api/6054d6/wines/${wineId}`
+      );
+      setData(response.data);
+    } catch (e) {
+      console.error(e);
+    }
+  }
 
+  useEffect(() => {
+    getWineByID();
+  }, []);
 
-    return (
-        <div>
-           <WineDetails name={data.name} year={data.year} grapes={data.grapes} country={data.country} region={data.region} desc={data.description} pic={data.picture} price={data.price} /> 
-        </div>
-        )
-}
+  return (
+    <div>
+        {/* { data ? <BookDetails book={ data } /> : "Retrieving book details..." } */}
+        { data ?( 
+      <WineDetails
+        name={data.name}
+        year={data.year}
+        grapes={data.grapes}
+        country={data.country}
+        region={data.region}
+        desc={data.description}
+        pic={data.picture}
+        price={data.price}
+      />)
+        : "Loading..."}
+    </div>
+  );
+};
 
 export default Wine;
