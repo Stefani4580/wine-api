c84e3e8c068af186558cd9f07470e660eba7c150
Stefani Baker <stefanibaker@MacBook-Air-4.local>
2020-08-03 20:27:14 -0400

    Ugly but working

--- src/components/WineDetails.js
+++ src/components/WineDetails.js
@@ -1,12 +1,16 @@
 import React from "react";
-import ListGroup from "react-bootstrap/ListGroup";
-import Card from "react-bootstrap/Card";
-import ListGroupItem from "react-bootstrap/ListGroupItem";
+// import ListGroup from "react-bootstrap/ListGroup";
+// import Card from "react-bootstrap/Card";
+// import ListGroupItem from "react-bootstrap/ListGroupItem";
 
 function WineDetails(props) {
   return (
     <div>
-      <Card style={{ width: "18rem" }}>
+        <h1>{props.wine.name}</h1>
+        <h2>{props.wine.year}</h2>
+        <p>{props.wine.description}</p>
+        {/* I tried to use React Bootstrap, but it wasn't working */}
+      {/* <Card style={{ width: "18rem" }}>
         <Card.Img variant="top" src={props.wine.picture} />
         <Card.Body>
           <Card.Title>{props.wine.name}</Card.Title>
@@ -19,7 +23,7 @@ function WineDetails(props) {
           <ListGroupItem>{props.wine.divregion}</ListGroupItem>
           <ListGroupItem>${props.wine.price}</ListGroupItem>
         </ListGroup>
-      </Card>
+      </Card> */}
     </div>
   );
 }
